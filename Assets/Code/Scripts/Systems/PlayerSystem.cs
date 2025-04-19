962fc9878c79c7bc95c2885bf9184fb4028522f2
Andrey Suvorov <andryusha.suvorov.05@mail.ru>
2025-04-19 16:18:43 +0300

    +ui

--- Assets/Code/Scripts/Systems/PlayerSystem.cs
+++ Assets/Code/Scripts/Systems/PlayerSystem.cs
@@ -1,12 +1,16 @@
 using System;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerSystem : Singleton<PlayerSystem>
 {
     [SerializeField] private int Energy = 3;
     [SerializeField] private int Money = 100;
     [SerializeField, Range(0.0f, 2.0f)] private float ChangingDelay = 1.0f;
+
+    [SerializeField] public Text MoneyText;
+    [SerializeField] public Text EnergyText;
 
     public void Start()
     {
@@ -17,12 +21,14 @@ public class PlayerSystem : Singleton<PlayerSystem>
     {
         if (float.IsNaN(delay)) delay = ChangingDelay;
         DOTween.To(() => Energy, x => Energy = x, amount, delay);
+        EnergyText.SetText(Energy.ToString());
     }
 
     public void ChangeMoney(int amount, float delay = float.NaN)
     {
         if (float.IsNaN(delay)) delay = ChangingDelay;
         DOTween.To(() => Money, x => Money = x, amount, delay);
+        MoneyText.SetText(Money.ToString());
     }
 
     public int GetEnergy()

ef7a0c298001228b54faf506a131de3bc31d2d75
Andrey Suvorov <andryusha.suvorov.05@mail.ru>
2025-04-23 18:38:38 +0300

    disaster randomize

--- Assets/Scripts/Systems/DisasterSystem.cs
+++ Assets/Scripts/Systems/DisasterSystem.cs
@@ -1,8 +1,36 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DisasterSystem : MonoBehaviour
 {
+    [SerializeField] private DisasterSO disasterSO;
+
+    private List<DisasterData> disasters;
+
+    private List<DisasterData> disastersRandom = new List<DisasterData>(); // ingame meow meow
+
+    void Start()
+    {
+        if (disasterSO != null) disasters = new List<DisasterData>(disasterSO.disasterData);
+        Randomize();
+        foreach (var i in disastersRandom) {
+            if (i == null) print("null");
+            print(i.description);
+        }
+        
+    }
+    public void Randomize()
+    {
+        int counter = disasters.Count;
+        for (int i = 0; i < counter; ++i) {
+            int random = UnityEngine.Random.Range(0, disasters.Count);
+            disastersRandom.Add(disasters[random]);
+            disasters.RemoveAt(random);
+        }
+        disasters = new List<DisasterData>(disasterSO.disasterData);
+    }
+
     void OnEnable()
     {
         ActionSystem.AttachPerformer<DisasterGA>(EnemyTurnPerformer);
